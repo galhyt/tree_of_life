bb3ce10f58d60063118b0fa80b5924ae0154aecd
Gal Hayat <gal.hayat@zesty.co>
2022-05-02 22:13:29 +0300

    move helper functions to a new file and namespace

--- src/tree_of_life/game.clj
+++ src/tree_of_life/game.clj
@@ -1,34 +1,8 @@
-(ns tree-of-life.game
-  (:import (clojure.lang PersistentList)))
+(ns tree-of-life.game)
 (require '[clojure.string :as str]
-         '[clojure.edn :as edn])
-
-(defn get-node-value [tree-state path]
-  ; Helper function
-  (get tree-state path))
-
-(defn get-neighbours-paths [path]
-  ; Helper function
-  (let [parent (if (not= path []) (drop-last path) nil)
-        left (conj path "<")
-        right (conj path ">")]
-    [parent left right]))
-
-(defn get-node-new-state [tree-state rule path]
-  ; Helper function
-  (let [neighbours (get-neighbours-paths path)]
-    (get rule (str/join "" (map #(if (= % nil) "." (get-node-value tree-state %))
-                                (conj (into [] (take 2 neighbours)) path (last neighbours)))))))
-
-(defn get-tree-new-state [tree-state rule neighbours-depth path]
-  ; Helper function
-  ; Gets new state of path node and its neighbours and their neighbours and so on, according to neighbours-depth:
-  ;  for neighbours-depth = 1 - path node and its neighbours, 2 - section 1 and path node neighbours neighbours and so on
-  (into {} (for [nodep (distinct (concat (take neighbours-depth
-                                               (iterate #(concat (for [x %] (remove nil? (get-neighbours-paths x))))
-                                                        (conj (remove nil? (get-neighbours-paths path)) path)))))]
-             {nodep (get-node-new-state tree-state rule nodep)})))
-
+         '[clojure.edn :as edn]
+         '[tree-of-life.helper :refer [get-node-value get-node-new-state get-tree-new-state
+                                       get-rule-struct get-tree-state-struct]])
 
 (defn query [tree-state rule iterations-number path]
    (if (= iterations-number 0)
@@ -38,37 +12,6 @@
        (let [neighbours-depth (dec iterations-number)
              tree-new-state (get-tree-new-state tree-state rule neighbours-depth path)]
            (recur tree-new-state rule (dec iterations-number) path)))))
-
-(defn get-rule-struct [rule-code]
-  ; Helper function
-  (let [bin-vals (str/replace (str/replace
-                                (format "%16d" (Long/parseLong (Long/toString rule-code 2)))  "1" "x") #"[\s0]" ".")
-        bin-keys (map #(str/replace (str/replace (format "%4d" (Integer/parseInt %)) "1" "x") #"[\s0]" ".")
-                      (for [i (range 15 -1 -1)] (Long/toString i 2)))]
-    (into {} (map (fn [k v] {k v}) bin-keys bin-vals))))
-
-(defn get-tree-state-struct [tree-state-str]
-  ; Helper function
-  (let [tree-state (edn/read-string tree-state-str)
-        tree-depth (first (first (filter #(not= (type (second %)) PersistentList)
-                                         (iterate #(let [index (first %) branch (second %)]
-                                                     [(inc index) (first branch)]) [1 tree-state]))))]
-    (into {}
-          (let [paths
-                (apply concat (take tree-depth
-                                    (iterate #(apply concat (map (fn [path] [(conj path "<") (conj path ">")]) %)) [[]])))]
-            (for [path paths]
-              (if (= path [])
-                {path (second tree-state)}
-                {path (second (nth
-                                (iterate #(let [index (first %) branch (last %) dir (path index)]
-                                            (if (= index (count path))
-                                              [(inc index) (if (= (type branch) PersistentList) (second branch) branch)]
-                                              (if (= dir "<")
-                                                [(inc index) (first branch)]
-                                                (if (= dir ">")
-                                                  [(inc index) (last branch)]))))
-                                         [0 tree-state]) (count path)))}))))))
 
 (defn game-of-life
   ; Takes raw inputs and return results
@@ -99,5 +42,3 @@
              iterations-number (Integer/parseInt (first query-str-arr))
              query-path (second query-str-arr)]
         (game-of-life input-tree-start-state input-rule-code iterations-number query-path)))))
-
-(play-the-game-of-life)
---
+++ src/tree_of_life/helper.clj
@@ -0,0 +1,61 @@
+(ns tree-of-life.helper
+  (:import (clojure.lang PersistentList)))
+(require '[clojure.string :as str]
+         '[clojure.edn :as edn])
+
+(defn get-node-value [tree-state path]
+      ; Helper function
+      (get tree-state path))
+
+(defn get-neighbours-paths [path]
+      ; Helper function
+      (let [parent (if (not= path []) (drop-last path) nil)
+            left (conj path "<")
+            right (conj path ">")]
+           [parent left right]))
+
+(defn get-node-new-state [tree-state rule path]
+      ; Helper function
+      (let [neighbours (get-neighbours-paths path)]
+           (get rule (str/join "" (map #(if (= % nil) "." (get-node-value tree-state %))
+                                       (conj (into [] (take 2 neighbours)) path (last neighbours)))))))
+
+(defn get-tree-new-state [tree-state rule neighbours-depth path]
+      ; Helper function
+      ; Gets new state of path node and its neighbours and their neighbours and so on, according to neighbours-depth:
+      ;  for neighbours-depth = 1 - path node and its neighbours, 2 - section 1 and path node neighbours neighbours and so on
+      (into {} (for [nodep (distinct (concat (take neighbours-depth
+                                                   (iterate #(concat (for [x %] (remove nil? (get-neighbours-paths x))))
+                                                            (conj (remove nil? (get-neighbours-paths path)) path)))))]
+                    {nodep (get-node-new-state tree-state rule nodep)})))
+
+(defn get-rule-struct [rule-code]
+      ; Helper function
+      (let [bin-vals (str/replace (str/replace
+                                    (format "%16d" (Long/parseLong (Long/toString rule-code 2)))  "1" "x") #"[\s0]" ".")
+            bin-keys (map #(str/replace (str/replace (format "%4d" (Integer/parseInt %)) "1" "x") #"[\s0]" ".")
+                          (for [i (range 15 -1 -1)] (Long/toString i 2)))]
+           (into {} (map (fn [k v] {k v}) bin-keys bin-vals))))
+
+(defn get-tree-state-struct [tree-state-str]
+      ; Helper function
+      (let [tree-state (edn/read-string tree-state-str)
+            tree-depth (first (first (filter #(not= (type (second %)) PersistentList)
+                                             (iterate #(let [index (first %) branch (second %)]
+                                                            [(inc index) (first branch)]) [1 tree-state]))))]
+           (into {}
+                 (let [paths
+                       (apply concat (take tree-depth
+                                           (iterate #(apply concat (map (fn [path] [(conj path "<") (conj path ">")]) %)) [[]])))]
+                      (for [path paths]
+                           (if (= path [])
+                             {path (second tree-state)}
+                             {path (second (nth
+                                             (iterate #(let [index (first %) branch (last %) dir (path index)]
+                                                            (if (= index (count path))
+                                                              [(inc index) (if (= (type branch) PersistentList) (second branch) branch)]
+                                                              (if (= dir "<")
+                                                                [(inc index) (first branch)]
+                                                                (if (= dir ">")
+                                                                  [(inc index) (last branch)]))))
+                                                      [0 tree-state]) (count path)))}))))))
