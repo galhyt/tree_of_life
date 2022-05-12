e4d2bb614e7862b9556bb7953f7fdb2e41b3a15a
Gal Hayat <gal.hayat@zesty.co>
2022-05-13 01:03:54 +0300

    unit testing

--- src/tree_of_clj/helper.clj
+++ src/tree_of_clj/helper.clj
@@ -1,36 +1,39 @@
-(ns tree-of-clj.helper
-  (:require))
+(ns tree-of-clj.helper)
 
-(def * ".")
+(def o ".")
 (def x "x")
 (def < "<")
 (def > ">")
 
 (defn get-value-by-path [tree path]
-  (if (empty path)
-    (second tree)
+  (if (empty? path)
+    (if (seq? tree) (second tree) tree)
     (case (first path)
-      > (get-value-by-path (last tree) (rest path))
-      < (get-value-by-path (first tree) (rest path)))))
+      ">" (get-value-by-path (last tree) (rest path))
+      "<" (get-value-by-path (first tree) (rest path)))))
 
 (defn- get-node-value [node]
-  (if (seq? node)
-    (get-node-value (second node))
-    node))
+  (if (= node nil)
+    o
+    (if (seq? node)
+      (get-node-value (second node))
+      node)))
 
 (defn iterate-on-tree
   ([tree rule]
-   (iterate-on-tree tree rule *))
+   (iterate-on-tree tree rule o))
   ([tree rule parent-value]
-   (let [left-child (first tree)
-         right-child (last tree)
-         self (second tree)
+   (let [left-child (if (seq? tree) (first tree) nil)
+         right-child (if (seq? tree) (last tree) nil)
+         self (if (seq? tree) (second tree) tree)
          current-node-value (get-node-value self)
          new-value (rule
                  parent-value
                  (get-node-value left-child)
                  current-node-value
                  (get-node-value right-child))]
-     '((iterate-on-tree left-child rule current-node-value)
-      new-value
-      (iterate-on-tree left-child rule current-node-value)))))
+     (if (not= left-child nil)
+       (list (iterate-on-tree left-child rule current-node-value)
+             new-value
+             (iterate-on-tree right-child rule current-node-value))
+       new-value))))
--- test/tree_of_clj/core_test.clj
+++ test/tree_of_clj/core_test.clj
@@ -1,11 +1,36 @@
 (ns tree-of-clj.core-test
   (:require
-    [midje.sweet :refer [fact facts =>]]
+    [clojure.test :refer :all]
+    [tree-of-clj.helper :refer [get-value-by-path < > o x
+                                iterate-on-tree]]))
 
-    [tree-of-clj.helper :refer [get-value-by-path < > * x]])
-  (:require [clojure.test :refer :all]
-            [tree-of-clj.core :refer :all]))
+(def trees ['(x o x)
+           '((x o x) o ((x x o) x o))])
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def rule-dict {'(x x x x) 'x
+     '(x x x o) 'o
+     '(x x o x) 'o
+     '(x x o o) 'x
+     '(x o x x) 'x
+     '(x o x o) 'o
+     '(x o o x) 'o
+     '(x o o o) 'x
+     '(o x x x) 'x
+     '(o x x o) 'o
+     '(o x o x) 'o
+     '(o x o o) 'x
+     '(o o x x) 'x
+     '(o o x o) 'o
+     '(o o o x) 'o
+     '(o o o o) 'x})
+
+(deftest get-value-by-path-test []
+    (is (= (get-value-by-path '(o x o) [>]) 'o))
+    (is (= (get-value-by-path '((x x o) x o) [<]) 'x))
+    (is (= (get-value-by-path '((x o x) o ((x x o) x o)) [> < >]) 'o)))
+
+(defn rule [p l c r]
+  (rule-dict '(p l c r)))
+
+(deftest iterate-on-tree-test []
+    (is (= (iterate-on-tree (trees 0) rule) '(o x x))))
